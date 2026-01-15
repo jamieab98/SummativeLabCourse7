17cbf1f9d6fba25f746ef0070e37e5681face812
Jamie Bryson <jamieab98@gmail.com>
2026-01-15 18:58:00 -0500

    Made it so each class creates the basic object and saves the object to the class list

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
+        self.projects=[]
         User.users.append(self)
 
     def __repr__(self):
@@ -19,6 +20,10 @@ class Project:
         self.title = title
         self.description = description
         self.due_date = due_date
+        Project.projects.append(self)
+    
+    def __repr__(self):
+        return self.title
 
 class Task:
     tasks = []
@@ -26,6 +31,11 @@ class Task:
         self.title = title
         self.assigned_to = []
         self.status = "incomplete"
+        Task.tasks.append(self)
+    
+    def __repr__(self):
+        return self.title
 
 user1 = User("Jamie", "jamieab98@gmail.com")
-User.show_users()
+
+project1 = Project("Classwork", "Complete Summative Lab", "09/09/1998")

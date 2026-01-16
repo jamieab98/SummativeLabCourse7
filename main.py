fa5990880cb4eb66b21dc58b868e6151a10960d9
Jamie Bryson <jamieab98@gmail.com>
2026-01-15 22:21:07 -0500

    created the projectdata.json file and connected it

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ add_user = subparsers.add_parser("add_user")
 add_user.add_argument("name", type=str)
 add_user.add_argument("email", type=str)
 
+create_project = subparsers.add_parser("create_project")
+create_project.add_argument("title", type=str)
+create_project.add_argument("description", type=str)
+create_project.add_argument("user", type=str)
+create_project.add_argument("due_date", type=str)
+
 list_projects = subparsers.add_parser("list_projects")
 list_projects.add_argument("user", type=str)
 
@@ -26,6 +32,15 @@ if args.command == "add_user":
         data = json.load(f)
     data.append(dict)
     with open("data/userdata.json", "w") as f:
+        json.dump(data, f, indent=2)
+
+if args.command == "create_project":
+    p = Project(args.title, args.description, args.due_date)
+    dict = {"title": p.title, "description": p.description, "due_date": p.due_date, "tasks": [], "users": []}
+    with open("data/projectdata.json", "r") as f:
+        data = json.load(f)
+    data.append(dict)
+    with open("data/projectdata.json", "w") as f:
         json.dump(data, f, indent=2)
 
 if args.command == "list_projects":

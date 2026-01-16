f076a4a6c0e980dc4f37e9a542d7da8c7cce1a97
Jamie Bryson <jamieab98@gmail.com>
2026-01-15 19:31:17 -0500

    Connected the user class to the project class and the project class to the task class

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 class User:
     users = []
-
     def __init__(self, name, email):
         self.name = name
         self.email = email
@@ -10,9 +9,8 @@ class User:
     def __repr__(self):
         return self.name
     
-    def show_users():
-        for user in User.users:
-            print(user)
+    def assign_project(self, project):
+        self.projects.append(project)
 
 class Project:
     projects = []
@@ -20,17 +18,22 @@ class Project:
         self.title = title
         self.description = description
         self.due_date = due_date
+        self.tasks = []
         Project.projects.append(self)
     
     def __repr__(self):
         return self.title
+    
+    def assign_task(self, task):
+        self.tasks.append(task)
 
 class Task:
     tasks = []
-    def __init__(self, title):
+    def __init__(self, title, project):
         self.title = title
-        self.assigned_to = []
         self.status = "incomplete"
+        project.tasks.append(self)
+
         Task.tasks.append(self)
     
     def __repr__(self):
@@ -39,3 +42,13 @@ class Task:
 user1 = User("Jamie", "jamieab98@gmail.com")
 
 project1 = Project("Classwork", "Complete Summative Lab", "09/09/1998")
+project2 = Project("Career", "Get a new Job", "1/1/2027")
+
+task1 = Task("Create the Models", project1)
+task2 = Task("Create the logic for the script", project1)
+task3 = Task("Complete Flatiron Software Development Bootcamp", project2)
+task4 = Task("Apply for coding classes at work", project2)
+task5 = Task("Make more money", project2)
+
+user1.assign_project(project1)
+user1.assign_project(project2)
